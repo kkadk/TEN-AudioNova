0c8b7669074b90f91fcdb8a907c9bf5c572a84cb
suryansh2121 <rajputsuryansh491@gmail.com>
2025-04-29 17:51:43 +0530

    work assigned

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+// suryansh 
 import React from 'react';
 
 export const Home = () => {
---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,2 @@
+// ( _ )  ye whatsapp se le rha hoon apka naam mujhe abhi pata nahi hai maine poocha bhi apse lekin aap is par kaar kar sakte ho
+// apko signup page bhi banana hai
