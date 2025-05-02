6b1bc9a6abca42aad029356ec91804645f92ade1
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-05-02 13:23:29 +0530

    library added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { Home } from './pages/Home';
-import { Footer } from './components/Footer';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Footer } from "./components/Footer";
+import Library from "./pages/Library";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
       </Routes>
-      
+
       <Footer />
     </BrowserRouter>
   );
--- Frontend/src/pages/Library.jsx
+++ Frontend/src/pages/Library.jsx
@@ -1,1 +1,47 @@
-// vikas chauhan
+import React from "react";
+import libraryData from "../assets/data/Library.js";
+
+function Library() {
+  return (
+    <div className="w-[20%] bg-purple-700 mt-2 ml-2 rounded-2xl">
+      <div className="text-white font-medium flex justify-between p-4">
+        <h1>Your Library</h1>
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl">
+          + Create
+        </button>
+      </div>
+
+      <div className="text-white font-medium px-4 pb-4">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl">
+          Playlist
+        </button>
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl ml-2">
+          Artist
+        </button>
+      </div>
+
+      <div>
+        {libraryData.map((data, index) => (
+          <div
+            key={index}
+            className="bg-purple-500 flex mx-2 mb-2 items-center p-2 rounded-lg"
+          >
+            <div className="w-20 h-20 flex-shrink-0">
+              <img
+                className="w-full h-full object-cover rounded-full"
+                src={data.image}
+                alt=""
+              />
+            </div>
+            <div className="ml-4 text-white">
+              <h2 className="text-lg font-semibold">{data.name}</h2>
+              <h3 className="text-sm">{data.role}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Library;
