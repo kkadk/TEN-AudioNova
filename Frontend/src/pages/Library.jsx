7f3c6908da3fbc5b8c9c20bd54b08e114893caba
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-05-02 13:58:32 +0530

    icons added in library page

--- Frontend/src/pages/Library.jsx
+++ Frontend/src/pages/Library.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import libraryData from "../assets/data/Library.js";
+import { FaPlus, FaSearch } from "react-icons/fa";
 
 function Library() {
   return (
     <div className="w-[20%] bg-purple-700 mt-2 ml-2 pb-2 rounded-2xl flex flex-col">
       <div className="text-white font-medium flex justify-between p-4">
         <h1>Your Library</h1>
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer">
-          + Create
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer flex items-center gap-1.5 hover:bg-purple-400">
+          <FaPlus /> Create
         </button>
       </div>
 
       <div className="text-white font-medium px-4 pb-4">
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer hover:bg-purple-400">
           Playlist
         </button>
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl ml-2 cursor-pointer">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl ml-2 cursor-pointer hover:bg-purple-400">
           Artist
         </button>
       </div>
 
-      {/* Scroll on hover */}
+      <div className="flex items-center w-40 h-10 text-white font-medium mx-4 mb-4 px-3 bg-purple-500 rounded-2xl hover:bg-purple-400 cursor-pointer">
+        <FaSearch className="mr-2 text-white" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-transparent outline-none text-white w-full placeholder-white"
+        />
+      </div>
+
       <div className="group relative h-[680px] px-2">
         <div className="overflow-hidden group-hover:overflow-y-scroll pr-2 h-full">
           {libraryData.map((data, index) => (
