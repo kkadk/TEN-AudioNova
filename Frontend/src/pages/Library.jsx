a9ac2ee92d8bbc4a3357558f6b4d86dfe18d2a0b
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-05-02 13:36:47 +0530

    library css improved

--- Frontend/src/pages/Library.jsx
+++ Frontend/src/pages/Library.jsx
@@ -3,42 +3,45 @@ import libraryData from "../assets/data/Library.js";
 
 function Library() {
   return (
-    <div className="w-[20%] bg-purple-700 mt-2 ml-2 rounded-2xl">
+    <div className="w-[20%] bg-purple-700 mt-2 ml-2 pb-2 rounded-2xl flex flex-col">
       <div className="text-white font-medium flex justify-between p-4">
         <h1>Your Library</h1>
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer">
           + Create
         </button>
       </div>
 
       <div className="text-white font-medium px-4 pb-4">
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl cursor-pointer">
           Playlist
         </button>
-        <button className="bg-purple-500 px-3 py-1 rounded-2xl ml-2">
+        <button className="bg-purple-500 px-3 py-1 rounded-2xl ml-2 cursor-pointer">
           Artist
         </button>
       </div>
 
-      <div>
-        {libraryData.map((data, index) => (
-          <div
-            key={index}
-            className="bg-purple-500 flex mx-2 mb-2 items-center p-2 rounded-lg"
-          >
-            <div className="w-20 h-20 flex-shrink-0">
-              <img
-                className="w-full h-full object-cover rounded-full"
-                src={data.image}
-                alt=""
-              />
+      {/* Scroll on hover */}
+      <div className="group relative h-[680px] px-2">
+        <div className="overflow-hidden group-hover:overflow-y-scroll pr-2 h-full">
+          {libraryData.map((data, index) => (
+            <div
+              key={index}
+              className="bg-purple-500 flex mb-2 items-center p-2 rounded-lg cursor-pointer hover:bg-purple-400"
+            >
+              <div className="w-20 h-20 flex-shrink-0">
+                <img
+                  className="w-full h-full object-cover rounded-full"
+                  src={data.image}
+                  alt=""
+                />
+              </div>
+              <div className="ml-4 text-white">
+                <h2 className="text-lg font-semibold">{data.name}</h2>
+                <h3 className="text-sm">{data.role}</h3>
+              </div>
             </div>
-            <div className="ml-4 text-white">
-              <h2 className="text-lg font-semibold">{data.name}</h2>
-              <h3 className="text-sm">{data.role}</h3>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
