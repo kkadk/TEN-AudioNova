f80e7e437089162dd7e6b9e2c0e0c1cc3646aa09
Harshagarwal190 <hagarwal190@gmail.com>
2025-05-10 18:21:52 +0530

    harsh_playlist

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Search } from "./pages/Search";
 import Library from "./pages/Library";
 import Profile from "./pages/Profile";
 import ArtistDetails from "./pages/ArtistDetails";
+import PlayList from "./pages/PlayList";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/artist" element={<ArtistDetails />} />
+        <Route path="/playlist" element={<PlayList />} />
       </Routes>
 
       <Footer />
