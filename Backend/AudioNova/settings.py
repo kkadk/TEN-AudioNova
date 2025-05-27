a1382296ae64bf1f54611103a38d2db467e9a039
kkadk <125792968+kkadk@users.noreply.github.com>
2025-05-27 15:14:17 +0545

    inbuilt django user model usage

--- Backend/AudioNova/settings.py
+++ Backend/AudioNova/settings.py
@@ -156,8 +156,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-# AUTH_USER_MODEL = 'taskm.CustomUser'  # Replace 'yourapp' with the app name and model name of your custom user model if you have one.
-
 # JWT Authentication settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
